9e12dfe998f0bc8f8cc934a208482672d5008a41
Claes Persson <c@cp.se>
2023-12-26 18:39:19 +0100

    Adding chrono as dependency and updating versions.

--- src/api/status.rs
+++ src/api/status.rs
@@ -1,3 +1,5 @@
+use chrono::prelude::*;
+
 use actix_web::{
     error::ResponseError,
     get,
@@ -16,7 +18,24 @@ pub struct StatusTypeIdentifier {
     status_type: String,
 }
 
+#[derive(Serialize)]
+pub struct StatusTypeResponse {
+    status_type_response: String,
+    status_type_date: String
+}
+
 #[get("/api/v1/status/{status_type}")]
-pub async fn get_status(type_identifier: Path<StatusTypeIdentifier>) -> Json<String> {
-    return Json(type_identifier.into_inner().status_type);
+pub async fn get_status(type_identifier: Path<StatusTypeIdentifier>) -> Json<StatusTypeResponse> {
+    let status_type = type_identifier.into_inner().status_type;
+    let status_type_date = Local::now().to_string();
+    let status_type_response: String;
+    match status_type.as_str() {
+        "ping" => {
+            status_type_response = "pong".to_string();
+        }
+        _ => status_type_response = "unknown status type".to_string()
+    }
+
+    let resp = StatusTypeResponse{ status_type_response, status_type_date };
+    Json(resp)
 }
